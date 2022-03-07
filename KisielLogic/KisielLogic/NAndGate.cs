b9a97f7475654425976ad00061850c65707ebef4
Kisielekw <dawid.kisielewski-2021@hull.ac.uk>
2022-03-07 14:15:47 +0000

    Added new Gates

---
+++ KisielLogic/KisielLogic/NAndGate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisielLogic
+{
+    class NAndGate : Gate2Input
+    {
+        public NAndGate(string pName) : base(pName)
+        {
+
+        }
+
+        public override void Update()
+        {
+            bool state1 = inputGates[0].State;
+            bool state2 = inputGates[1].State;
+
+            state = !(state1 && state2);
+        }
+    }
+}
---
+++ KisielLogic/KisielLogic/NOrGate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisielLogic
+{
+    class NOrGate:Gate2Input
+    {
+        public NOrGate(string pName) : base(pName)
+        {
+
+        }
+
+        public override void Update()
+        {
+            state = !(inputGates[0].State || inputGates[1].State);
+        }
+    }
+}
