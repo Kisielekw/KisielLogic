32647d5465454c8c929cc866ccd2d66bd368e2e2
Kisielek <d.kisielewski-2021@hull.ac.uk>
2022-03-21 16:18:36 +0000

    no change

--- KisielLogic/KisielLogic/Program.cs
+++ KisielLogic/KisielLogic/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace KisielLogic
 {
@@ -6,7 +10,7 @@ namespace KisielLogic
     {
         static void Main()
         {
-            
+
         }
     }
 }
