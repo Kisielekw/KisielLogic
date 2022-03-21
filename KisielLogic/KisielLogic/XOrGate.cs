9d47c827e9a406442f7cf28236dfd5e47f826f1f
Kisielek <d.kisielewski-2021@hull.ac.uk>
2022-03-21 17:09:47 +0000

    added XOrGate

---
+++ KisielLogic/KisielLogic/XOrGate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KisielLogic
+{
+    class XOrGate:Gate2Input
+    {
+        public XOrGate(string pName) : base(pName)
+        {
+
+        }
+
+        public override void Update()
+        {
+            state = inputGates[0].State ^ inputGates[1].State;
+            UpdateConnected();
+        }
+    }
+}
